8a23434596db79bc8831c3fad2a580cf1215dd7d
Anastasia-Dmv <Vesta50000@gmail.com>
2020-09-03 16:10:01 +0300

    ready 7 hw

---
+++ build/precache-manifest.515f79eff60b03133789c7bb7f962120.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc0d3ed3cd410b436d18fca767d5e173",
+    "url": "/index.html"
+  },
+  {
+    "revision": "949366d34c62d58287ce",
+    "url": "/static/css/main.45a24955.chunk.css"
+  },
+  {
+    "revision": "aa0b9c1ec4e6d7a433f9",
+    "url": "/static/js/2.bd2e5d34.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bd2e5d34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "949366d34c62d58287ce",
+    "url": "/static/js/main.df4b7248.chunk.js"
+  },
+  {
+    "revision": "509037c70ced1433267c",
+    "url": "/static/js/runtime-main.36888773.js"
+  }
+]);
